b63ac0ab926e3b2492dc7867711e9c9fb75755b6
Daniel Molina <dmolcap@gmail.com>
2021-10-07 17:08:26 +0200

    Add Inbox contract and compile script

---
+++ contracts/Inbox.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.17;
+
+contract Inbox {
+    // When we create a storage public variable, solidity automatically creates a function with the same name
+    // which returns the variable value
+    string public message;
+    
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+    
+    function setMessage(string newMessage) public returns(string) {
+        message = newMessage;
+    }
+    
+    // Since we declared the above message variable,
+    // we don't need the getMessage function, it's repetitive
+    // function getMessage() public view returns(string) {
+    //     return message;
+    // }
+    
+    function doMath(int a, int b) {
+        a + b;
+        b - a;
+        a * b;
+        a == 0;
+    }
+}
