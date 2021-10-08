b3a73aca24a4b580f2ccf7b618989a2c554c170d
Daniel Molina <dmolcap@gmail.com>
2021-10-08 14:08:25 +0200

    Add mocha test structure and deployment with web3

--- compile.js
+++ compile.js
@@ -1,5 +1,5 @@
-const path = require('path'); // build-in Node module, it generates a path which works either unix and windows systems
-const fs = require('fs'); // build-in Node module, fs = file system
+const path = require('path'); // built-in Node.js module, it generates a path which works either unix and windows systems
+const fs = require('fs'); // built-in Node.js module, fs = file system
 const solc = require('solc'); // Solidity compiler module
 const { compile } = require('solc');
 
@@ -7,6 +7,5 @@ const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); // __dirnam
 const source = fs.readFileSync(inboxPath, 'utf-8'); // it reads the raw source content of the .sol files with the specific encoding utf-8
 
 module.exports = solc.compile(source, 1).contracts[':Inbox']; // we pass the source code that we want to compile as first parameter, and the number of contracts that we'll compile as second parameter
-
 // For testing, run 'node compile.js' in the temrinal with:
 // console.log(solc.compile(source, 1));
---
+++ test/Inbox.test.js
@@ -0,0 +1,67 @@
+const assert = require('assert'); // built-in Node.js module
+const ganache = require('ganache-cli'); // it's gona serves as our local ethereum test network
+const Web3 = require('web3'); // Web3 is always used as constructor function, we'll always create a new instance of Web3 each time we work with it (reason because it starts with capital letters)
+const web3 = new Web3(ganache.provider()); // this creates an instance of Web3 and feed it with ganache as test network in our local, we'll change this provider depending on which network we want to connect to (Ethereum, Rinkeby, Ropsten, etc)
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+   // Get a list of all accounts
+   accounts = await web3.eth.getAccounts()
+
+   // Use one of those accounts to deploy the contract
+   // The below 'inbox' object is a JavaScript representation of our contract that will be deployed into the blockchain
+   inbox = await new web3.eth.Contract(JSON.parse(interface)) // Tells web3 that there's a Ethereum contract and the interface to interact with
+      .deploy({ // Creates a transaction object. Tells web3 that we want to deploy a new copy of this contract. And we pass the contract data/code (bytecode) and the arguments for the constructor that we need to initialitate the contract (initialMessage in this case)
+         data: bytecode,
+         arguments: ['Hello World'] // If we'd have a second parameter in the constructor function, we'd pass it as second argument inside this array
+      })
+      .send({ // Tells web3 to comunicate with the network and to send out this transaction to create the contract 
+         from: accounts[0],
+         gas: '1000000'
+      })
+});
+
+describe('Inbox', () => {
+   it('deploys a contract', () => {
+      console.log(inbox);
+   })
+})
+
+
+
+
+
+
+
+
+
+// MOCHA TEST EXAMPLE
+
+// class Car {
+//    park() {
+//       return 'stopped';
+//    }
+
+//    drive() {
+//       return 'vroom';
+//    }
+// }
+
+// let car; // we have to declare the variable outside beforeEach in order to the rest of test functions have access to it
+
+// beforeEach(() => { // it runs before each test function (it)
+//    car = new Car();
+// })
+
+// describe('Car', () => {
+//    it('Car can park', () => {
+//        assert.equal(car.park(), 'stopped');
+//    });
+
+//    it('Car can drive', () => {
+//       assert.equal(car.drive(), 'vroom');
+//    });
+// });
